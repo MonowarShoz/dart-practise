fed6d79adf17517b1d05eab0c3996d90662e6757
MonowarShoz <shozolhorn@gmail.com>
2022-08-18 13:12:38 +0600

    new

---
+++ bin/Student_class.dart
@@ -0,0 +1,12 @@
+
+class Student {
+  final String id;
+  final String name;
+  final String? address;
+
+  Student({
+    required this.id,
+    required this.name,
+    this.address,
+  });
+}
--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,6 +1,8 @@
 import 'package:dart_application_1/dart_application_1.dart'
     as dart_application_1;
 
+import 'Student_class.dart';
+
 class SampleClass {
   final int? a;
   final double? num1;
@@ -16,6 +18,46 @@ class SampleClass {
 void main() {
   SampleClass obj1 = SampleClass(a: 3, b: 'Cat', num1: 2.3);
   SampleClass obj2 = SampleClass(b: 'Dog');
+
+  Student student = Student(id: '1', name: 'Rohim');
+
+  //print(student.name);
+
+  List<Student> studentList = [];
+
+  studentList.add(Student(
+    id: '2',
+    name: 'Alex',
+  ));
+  studentList.insert(1, Student(id: '3', name: 'John'));
+  studentList.add(Student(
+    id: '4',
+    name: 'Korim',
+  ));
+  studentList.add(Student(
+    id: '5',
+    name: 'Rokib',
+  ));
+  studentList.add(Student(
+    id: '6',
+    name: 'Jamal',
+  ));
+
+  var list = [1, 2, 3, 4, 5];
+  list.where((element) => element < 3);
+  list.forEach((element) {
+    print('$element');
+  });
+
+  studentList.where((element) => element.name == 'Alex').toList();
+  //studentList.removeAt(1);
+//  studentList.removeRange(0, 3);
+
+  studentList.isEmpty ? print('No data') : print('No empty list');
+
+  for (var ob in studentList) {
+    print('name : ${ob.name} ID : ${ob.id}');
+  }
 
   List<SampleClass> listdata = [
     SampleClass(a: 2, b: 'cat', num1: 2.3),
@@ -41,13 +83,13 @@ void main() {
     ],
   );
 
-  for (var element in listdata) {
-    print(element.b);
-  }
+  // for (var element in listdata) {
+  //   print(element.b);
+  // }
 
-  listdata.forEach((item) {
-    print(item.num1);
-  });
+  // listdata.forEach((item) {
+  //   print(item.num1);
+  // });
 
   var sum = obj1.a! * obj1.a! * obj1.num1!;
 
@@ -55,9 +97,20 @@ void main() {
 
   var d = obj2.b;
 
-  final list = ['1', '2', '3', 4];
+  // final list = ['1', '2', '3', 4];
 
-  list.add('2');
+  // list.add('2');
+
+  // final List<SampleClass> newList = [];
+  // //final map = {};
+
+  // newList.add(SampleClass(a: 2, b: '3'));
+
+  // newList.forEach((element) {
+  //   return print(element.a);
+  // });
+
+  // print()
 
   // final Map<Object, Object> sampleMap = {
   //   'animal': ['tiger', 'elephant', 'cow'],
@@ -79,4 +132,13 @@ void main() {
   // }
 
   //print('$c $d $sum ${obj1.show()}');
+
+  final Map<String, Student> newMap = {
+    '1': Student(id: '2', name: 'Shj'),
+    '2': Student(id: '3', name: 'Rohim'),
+  };
+
+  newMap.forEach((key, value) {
+    return print(newMap['1']!.name);
+  });
 }
